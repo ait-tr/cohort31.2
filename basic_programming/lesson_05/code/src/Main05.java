6473546c88cc1274fbc4c195b7e5837280c8d810
Sergii Bugaienko <sergiibugaienko@gmail.com>
2023-07-22 10:00:50 +0200

    Lesson_05 add

---
+++ basic_programming/lesson_05/code/src/Main05.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class Main05 {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int random1 = random.nextInt(10);
+        System.out.println(random1);
+    }
+}
