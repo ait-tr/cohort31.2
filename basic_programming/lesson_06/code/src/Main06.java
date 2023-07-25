20510e7837982ab751050bdbe333c36d6440d373
Sergii Bugaienko <sergiibugaienko@gmail.com>
2023-07-25 10:53:24 +0200

    Update Main06.java

--- basic_programming/lesson_06/code/src/Main06.java
+++ basic_programming/lesson_06/code/src/Main06.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 
 public class Main06 {
     public static void main(String[] args) {
@@ -6,10 +5,40 @@ public class Main06 {
 
         {
             int y = 15; // переменная y объявлена в блоке
-             x = x + y;
+            x = x + y;
         }
 
         System.out.println(x); // x => 25
 //        x = x + y; // ошибка компиляции
+        {
+            int a = 10;
+            int b = 5;
+            System.out.println(a == b);
+
+            String str1 = "Java 9";
+            String str2 = "Java 9";
+            String str3 = new String("JAVA 9");
+
+            boolean b1 = str1 == str2;
+            System.out.println(b1);
+
+            System.out.println(str1);
+            System.out.println(str3);
+
+            boolean b2 = str1 == str3;
+            System.out.println("str1 == str3 " + b2);
+
+            boolean b3 = str1.equals(str3);
+            System.out.println("str1.equals(str3) " + b3);
+            boolean b4 = str1.equalsIgnoreCase(str3);
+            System.out.println("str1.equalsIgnoreCase(str3) " + b4);
+
+        }
+
+        {
+
+        }
+
+
     }
 }
