da9acca556bea1e2e93ef8b6b6dbcf463876ac74
Sergii Bugaienko <sergiibugaienko@gmail.com>
2023-08-16 13:01:29 +0200

    Lesson_22 code add

---
+++ basic_programming/lesson_22/code/src/Passenger.java
@@ -0,0 +1,17 @@
+public class Passenger extends Person {
+
+    static private int counter;
+    private int id;
+    public Passenger(String name) {
+        super(name);
+        this.id = counter++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String toString() {
+        return "{Passenger id: " + id +"; name: " + getName() + "}";
+    }
+}
---
+++ basic_programming/lesson_22/code/src/Train.java
@@ -0,0 +1,9 @@
+// Если мы задействуем механизм наследования,
+// Классы ОБЯЗАТЕЛЬНО должны иметь связь IS-A
+public class Train extends Vehicle{
+    public Train(String model, int yearOfManufacture) {
+        super(model, yearOfManufacture);
+    }
+
+
+}
