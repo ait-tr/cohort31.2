e83b38a9f2db978bde2ff6fc905abed73bbf2f79
Evgenygroup <mailfrom61@gmail.com>
2023-09-27 14:17:23 +0200

    adds lesson 52

---
+++ basic_programming/lesson_52/code/byte_streams/src/Employee.java
@@ -0,0 +1,15 @@
+public class Employee {
+
+    public String name;
+    public int age;
+    public double salary;
+
+    public boolean isManager;
+
+    public Employee(String name, int age, double salary, boolean isManager) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+        this.isManager = isManager;
+    }
+}
---
+++ basic_programming/lesson_52/code/stream_closing/src/Main.java
@@ -0,0 +1,49 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Main {
+// закрытие  ресурса с помощью try catch finally
+    public static void main(String[] args) {
+        FileInputStream fileInputStream = null;
+        try {
+             fileInputStream = new FileInputStream("first.txt");
+
+            int i;
+            while ((i = fileInputStream.read()) != -1){
+                System.out.print((char)i);
+            }
+        }
+        catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+        finally {
+            try {
+                if (fileInputStream != null)
+                    fileInputStream.close();
+            }
+            catch (IOException e){
+                System.out.println(e.getMessage());
+            }
+        }
+
+        // try-with-resources - в этом случае метод close  вызывается автоматически.
+        System.out.println("\nwith try-with-resources");
+
+        try(FileInputStream fileInputStream1 = new FileInputStream("first.txt")) {
+
+            int i;
+            while ((i = fileInputStream1.read()) != -1){
+                System.out.print((char)i);
+            }
+        }
+        catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+
+
+        }
+
+
+    }
+
