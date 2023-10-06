0dc09dcc58fe0b87cb91e259fc9bd63e22014000
Evgenygroup <mailfrom61@gmail.com>
2023-10-06 14:32:52 +0200

    adds lesson 58

---
+++ basic_programming/lesson_58/code/practice58/src/WordTransformerStar.java
@@ -0,0 +1,11 @@
+public class WordTransformerStar extends WordsTransformer{
+    @Override
+    public boolean check(String input) {
+        return input.length() == 5;
+    }
+
+    @Override
+    public String transform(String string) {
+        return "*****";
+    }
+}
