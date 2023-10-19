3e926ce47a55deabcd0c606eb8ea82b16130d66c
Evgenygroup <mailfrom61@gmail.com>
2023-10-19 14:06:24 +0200

    adds lesson 67

---
+++ basic_programming/lesson_67/code/practice67/src/RegularClark.java
@@ -0,0 +1,21 @@
+public class RegularClark extends AbstractClerk{
+
+    public RegularClark( String name, int minTime, int maxTime, int creditsNum){
+        super(name,minTime,maxTime,creditsNum);
+    }
+
+
+    @Override
+    public void run() {
+        for(int i = 0; i < creditsNum; i++){
+            int toSleep = random.nextInt(maxTime - minTime) + minTime;
+            try {
+                Thread.sleep(toSleep);
+            } catch (InterruptedException e){
+                return;
+            }
+        }
+        finishTime = System.currentTimeMillis();
+
+    }
+}
---
+++ basic_programming/lesson_67/code/traffic_light/src/TrafficLightColour.java
@@ -0,0 +1,5 @@
+public enum TrafficLightColour {
+    RED,
+    GREEN,
+    YELLOW
+}
