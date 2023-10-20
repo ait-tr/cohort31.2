3837b2d02fdbf02d7cef39219f11ae362abf938a
Evgenygroup <mailfrom61@gmail.com>
2023-10-20 13:24:21 +0200

    adds lesson 68

---
+++ basic_programming/lesson_68/code/tarakans/src/Score.java
@@ -0,0 +1,13 @@
+public class Score {
+    String name;
+    long time;
+
+    public Score(String name, long time) {
+        this.name = name;
+        this.time = time;
+    }
+
+    public String toString(){
+        return name + " ran the distance for " + time + " milliseconds\n";
+    }
+}
