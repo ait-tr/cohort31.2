e798fde4bc9276f8c3f54f866ebfd6d2c20ec838
v1148656 <v1148656@gmail.com>
2023-11-29 18:08:27 +0200

    Add lesson_19

---
+++ front_end/lesson_19/code/19_props_hooks_29_11_2023/src/components/JsonTodos.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+
+interface ITodo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const JsonTodos = () => {
+  const [todoList, setTodoList] = useState<ITodo[] | null>(null);
+
+  useEffect(() => {
+    // console.log('Component did mount');
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((data) => setTodoList(data));
+  });
+
+  return (
+    <>
+        {
+            todoList && 
+            todoList.map(todo => (
+                <>
+                    {todo.title} {todo.completed}
+                </>
+            ))          
+        }
+    </>
+  );
+};
+
+export default JsonTodos;
