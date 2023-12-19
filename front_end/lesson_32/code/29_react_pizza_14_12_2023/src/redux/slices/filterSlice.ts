2fe6a1b0a5a1fb23d2f93385e2ddc575c61d0a2e
v1148656 <v1148656@gmail.com>
2023-12-19 15:26:13 +0200

    Add lesson_32

---
+++ front_end/lesson_32/code/29_react_pizza_14_12_2023/src/redux/slices/filterSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit"
+import { RootState } from "../store";
+
+const initialState = {
+    categoryId: 0,
+    categoryNames: [
+        'Все',
+        'Мясные',
+        'Вегетарианские',
+        'Гриль',
+        'Острые',
+        'Закрытые'
+    ],
+    categoryName: 'Все',
+    sortingItem: {
+        name: 'популярности',
+        sortProperty: 'rating',
+        order: 'desc'
+    },
+    currentPage: 1
+}
+
+export const filterSlice = createSlice({
+    name: 'filter',
+    initialState,
+    reducers: {
+        onClickCategory(state, action) {
+            state.categoryId = action.payload;
+            state.categoryName = state.categoryNames[state.categoryId];
+        },
+        onClickSort(state, action) {
+            state.sortingItem = action.payload;
+        },
+        setCurrentPage(state, action) {
+            state.sortingItem = action.payload;
+        }
+    }
+});
+
+export const selectFilter = (state: RootState) => state.filter;
+
+export const { onClickCategory, onClickSort, setCurrentPage } = filterSlice.actions;
+
+export default filterSlice.reducer;
