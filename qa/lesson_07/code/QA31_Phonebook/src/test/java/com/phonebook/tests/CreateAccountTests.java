9f7e9b711bb834f1b93b6df05e8d7adae6312a01
Ira Baker <49585685+irinabaker@users.noreply.github.com>
2024-02-29 13:26:53 +0100

    Lesson_07

--- qa/lesson_07/code/QA31_Phonebook/src/test/java/com/phonebook/tests/CreateAccountTests.java
+++ qa/lesson_07/code/QA31_Phonebook/src/test/java/com/phonebook/tests/CreateAccountTests.java
@@ -1,5 +1,6 @@
 package com.phonebook.tests;
 
+import com.phonebook.models.User;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -8,20 +9,20 @@ public class CreateAccountTests extends TestBase{
 
     @BeforeMethod
     public void ensurePrecondition() {
-        if (!isLoginLinkPresent()) {
-            clickOnSignOutButton();
+        if (!app.getUser().isLoginLinkPresent()) {
+            app.getUser().clickOnSignOutButton();
         }
     }
 
     @Test
     public void createExistedAccountNegativeTest() {
-        clickOnLoginLink();
-        fillLoginRegisterForm(new User()
+        app.getUser().clickOnLoginLink();
+        app.getUser().fillLoginRegisterForm(new User()
                 .setEmail("manuel@gm.com")
                 .setPassword("Manuel1234$"));
-        clickOnRegistrationButton();
+        app.getUser().clickOnRegistrationButton();
         //assert Sign Out button is present
-        Assert.assertTrue(isAlertAppears());
+        Assert.assertTrue(app.getUser().isAlertAppears());
     }
 
 }
---
+++ qa/lesson_07/homework.md
@@ -0,0 +1,17 @@
+## Homework
+
+Решите, пожалуйста, [задачи](https://docs.google.com/presentation/d/15s6oOpZxPW7tTGv33b-78iRRjSk8djL_/edit?usp=sharing&ouid=116447005932578256378&rtpof=true&sd=true), используя таблицы решений
+
+Скачайте, пожалуйста, [geckodriver](https://github.com/mozilla/geckodriver/releases) или [msedgedriver](https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/?form=MA13LH) и запустите домашний проект(предварительно превратив его в Value Object Model) через консоль, используя команду
+
+_gradle -Pbrowser=firefox clean qa_
+
+_gradle -Pbrowser=edge clean qa_
+
+дефолтная команда для запуска в Chrome всего набора тестов в проекте
+
+_gradle clean qa_
+
+[Microsoft Edge WebDriver](https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/?form=MA13LH)
+
+[MAC(JAVA_HOME; PATH)](https://docs.google.com/document/d/1D-aJJ1-kR3YajD9OXCOOO2MbCCTUa24GTID9FwcQJS8/edit)
